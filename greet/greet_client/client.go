11950ec8b685f17ee8915f2871470fb95330f73e
petros <pit.trak@gmail.com>
2021-02-06 18:19:56 +0200

    client setup boilerplate code

---
+++ greet/greet_client/client.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"gRPC-API-and-Microservices-with-Golang/greet/greetpb"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello, I'm a client!")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client: %f", c)
+}
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"gRPC-API-and-Microservices-with-Golang/greet/greetpb"
 	"log"
 	"net"
@@ -11,14 +12,16 @@ import (
 type server struct{}
 
 func main() {
+	fmt.Println("Hello, I'm a server!")
+
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	greetpb.RegisterGreetServiceServer(s, &server{})
 
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
