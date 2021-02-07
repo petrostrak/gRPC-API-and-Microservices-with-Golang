f61ea061c1e0aab0682f129a161a75d71dbd5f2c
petros <pit.trak@gmail.com>
2021-02-07 16:55:54 +0200

    Server Streaming API client impl

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"gRPC-API-and-Microservices-with-Golang/greet/greetpb"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
@@ -22,6 +23,34 @@ func main() {
 	// fmt.Printf("Created client: %f", c)
 
 	doUnary(c)
+
+	doServerStreaming(c)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting Server Streaming RPC...")
+
+	req := &greetpb.GreetManyRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Petros",
+			LastName:  "Trak",
+		},
+	}
+	resStream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling GreetManyTimes RPC: %v", err)
+	}
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			// we've reached the end of the stream
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while reading stream: %v", err)
+		}
+		log.Println("Response from GreetingManyTimes", msg.GetResult())
+	}
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
