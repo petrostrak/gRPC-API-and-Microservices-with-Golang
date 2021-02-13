44eba5f3d0856fdd207ac735b94a311ffa09167d
petros <pit.trak@gmail.com>
2021-02-13 12:32:12 +0200

    ReadBlog client

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -16,7 +16,7 @@ func main() {
 
 	cc, err := grpc.Dial("localhost:50051", options)
 	if err != nil {
-		log.Fatalf("could not connect: %v", err)
+		log.Fatalf("could not connect: %v\n", err)
 	}
 	defer cc.Close()
 
@@ -32,7 +32,17 @@ func main() {
 	fmt.Println("Blog created")
 	cb, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
 	if err != nil {
-		log.Fatalf("unexpected error: %v", err)
+		log.Fatalf("unexpected error: %v\n", err)
 	}
-	fmt.Printf("Blog has been created: %v", cb)
+	fmt.Printf("Blog has been created: %v\n", cb)
+
+	// extract blogid from create blog response
+	blogID := cb.GetBlog().GetId()
+
+	// read Blog
+	res, err := c.ReadBlog(context.Background(), &blogpb.ReadBlogRequest{BlogId: blogID})
+	if err != nil {
+		fmt.Printf("error while reading: %v\n", err)
+	}
+	fmt.Printf("Blog was read: %v\n", res)
 }
